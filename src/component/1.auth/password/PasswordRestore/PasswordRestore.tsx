406369f51b34aa0ab3d26c71efa7635b4150e720
Vadzim Dzianisik <wadik.nhl@gmail.com>
2022-01-11 16:30:32 +0300

    add password restore component

--- src/component/1.auth/password/PasswordRestore/PasswordRestore.tsx
+++ src/component/1.auth/password/PasswordRestore/PasswordRestore.tsx
@@ -1,9 +1,32 @@
+import React, {FormEvent} from "react";
+import {Link} from "react-router-dom";
+
 const PasswordRestore = () => {
-    return (
-        <>
-            <span>PasswordRestore</span>
-        </>
-    )
+
+  const onsubmit = (e: FormEvent) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <h3>Forgot your password?</h3>
+      <form onSubmit={onsubmit}>
+        <div>
+          <label>
+            Email <br/>
+            <input type="text" name="email"/>
+          </label>
+        </div>
+        <p>Enter your email address and we will send you further instructions</p>
+        <div>
+          <button type="submit">Send Instructions</button>
+        </div>
+      </form>
+      <p>Did you remember your password?</p>
+      <Link to={'/login'}>Try logging in</Link>
+    </>
+  )
 }
+
 
 export default PasswordRestore
