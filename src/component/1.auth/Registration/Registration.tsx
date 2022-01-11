d5adb101edcba8681971d64f2a49c7581a916000
YK177 <kotenev.yuri@gmail.com>
2022-01-11 16:43:28 +0400

    add jsx markup

--- src/component/1.auth/Registration/Registration.tsx
+++ src/component/1.auth/Registration/Registration.tsx
@@ -1,7 +1,31 @@
 const Registration = () => {
     return (
         <>
-            <span>Registration</span>
+            <h3>Sign up</h3>
+            <form>
+                <div>
+                    <label>
+                        Email <br/>
+                        <input type="text" name="email"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password <br/>
+                        <input type="password" name="password"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Confirm password <br/>
+                        <input type="password" name="confirm"/>
+                    </label>
+                </div>
+                <div>
+                    <button type="reset">Cancel</button>
+                    <button type="submit">Register</button>
+                </div>
+            </form>
         </>
     )
 }
