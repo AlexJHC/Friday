21219a64d9a7466d4fe18dabbb1596a0d4c274c9
YK177 <kotenev.yuri@gmail.com>
2022-01-11 17:16:44 +0400

    add local state & form handlers

--- src/component/1.auth/Registration/Registration.tsx
+++ src/component/1.auth/Registration/Registration.tsx
@@ -1,24 +1,74 @@
+import {ChangeEvent, FormEvent, useState} from 'react'
+
 const Registration = () => {
+
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [confirm, setConfirm] = useState<string>('')
+
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        setEmail(e.currentTarget.value)
+    }
+
+    const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        setPassword(e.currentTarget.value)
+    }
+
+    const handleConfirmChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        setConfirm(e.currentTarget.value)
+    }
+
+    const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const formData = {
+            email: email,
+            password: password
+        }
+
+        resetForm()
+
+        console.log(formData)
+    }
+
+    const resetForm = () => {
+        setEmail('')
+        setPassword('')
+        setConfirm('')
+    }
+
     return (
         <>
             <h3>Sign up</h3>
-            <form>
+            <form onSubmit={handleFormSubmit} onReset={resetForm}>
                 <div>
                     <label>
                         Email <br/>
-                        <input type="text" name="email"/>
+                        <input onChange={handleEmailChange}
+                               type="text"
+                               placeholder="example@inbox.com"
+                               name="email"
+                               value={email}/>
                     </label>
                 </div>
                 <div>
                     <label>
                         Password <br/>
-                        <input type="password" name="password"/>
+                        <input onChange={handlePasswordChange}
+                               type="password"
+                               placeholder="********"
+                               name="password"
+                               value={password}/>
                     </label>
                 </div>
                 <div>
                     <label>
                         Confirm password <br/>
-                        <input type="password" name="confirm"/>
+                        <input onChange={handleConfirmChange}
+                               type="password"
+                               placeholder="********"
+                               name="confirm"
+                               value={confirm}/>
                     </label>
                 </div>
                 <div>
