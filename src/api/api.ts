9823f134d99cdaeb1d2e7a0f8ea79458796d7038
YK177 <kotenev.yuri@gmail.com>
2022-01-11 21:38:48 +0400

    add authAPI.register()

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,26 @@
-import axios from 'axios'
+import axios, {AxiosResponse} from 'axios'
+
+// const instance = axios.create({
+//     baseURL: 'https://neko-back.herokuapp.com/2.0/',
+//     withCredentials: true
+// })
 
 const instance = axios.create({
-    baseURL: 'https://neko-back.herokuapp.com/2.0/',
-    withCredentials: true
+    baseURL: 'http://localhost:7540/2.0/',
+    withCredentials: true,
 })
+
+export const authApi = {
+    register(data: RegisterDataType) {
+        return instance.post<RegisterDataType,AxiosResponse<RegisterResponseType>>('auth/register', data)
+    }
+}
+
+// Type
+type RegisterDataType = {
+    email: string
+    password: string
+}
+type RegisterResponseType={
+    error?:string
+}
