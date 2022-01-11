e180cdb62c2fcc849f1abf4b988ba118b2c28703
AlexJHC <shchepkin2021@gmail.com>
2022-01-11 13:25:02 +0200

    add jsx for login page

--- src/component/1.auth/login/Login.tsx
+++ src/component/1.auth/login/Login.tsx
@@ -1,8 +1,22 @@
+import style from './Login.module.css'
+import InputText from "../../3.features/InputText/InputText";
+import {Link} from "react-router-dom";
+import React from "react";
+import Button from "../../3.features/Button/Button";
+
 const Login = () => {
     return (
-        <>
-            <span>Login</span>
-        </>
+        <div className={style.container}>
+            <h1>Sign In</h1>
+            <InputText/>
+            <br/>
+            <InputText/>
+            <Link to={'/password-restore'}>Forgot Password</Link>
+            <Button>Login</Button>
+            <span>Don't have an account</span>
+            <Link to={'/registration'}>Sign Up</Link>
+
+        </div>
     )
 }
 
