e5c06190cdc5b00daa560613be04b1ced1755aac
YK177 <kotenev.yuri@gmail.com>
2022-01-13 14:49:53 +0400

    add initial state & types in profileReducer.ts

--- src/component/2.profile/profileReducer.ts
+++ src/component/2.profile/profileReducer.ts
@@ -1,9 +1,44 @@
-const profileInitState = {}
+const profileInitState = {
+  user: {
+    _id: '',
+    email: '',
+    name: '',
+    avatar: '',
+    publicCardPacksCount: 0,
+    created: new Date(),
+    updated: new Date(),
+    isAdmin: false,
+    verified: false,
+    rememberMe: false,
+    error: ''
+  } as ProfileType,
+}
 
-export const profileReducer = (state = profileInitState, action: any): any => {
+export const profileReducer = (state: InitStateType = profileInitState, action: ProfileActionsType): InitStateType => {
   switch (action.type) {
     default: {
       return state
     }
   }
 }
+
+// Actions
+
+// Thunk
+
+// Types
+type InitStateType = typeof profileInitState
+export type ProfileType = {
+  _id: string
+  email: string
+  name: string
+  avatar: string
+  publicCardPacksCount: number
+  created: Date
+  updated: Date
+  isAdmin: boolean
+  verified: boolean
+  rememberMe: boolean
+  error: string
+}
+type ProfileActionsType = any
