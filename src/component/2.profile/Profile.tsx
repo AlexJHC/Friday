1f250cd568750152be574ebf7ca1d12762533c34
YK177 <kotenev.yuri@gmail.com>
2022-01-13 14:30:07 +0400

    add JSX for Profile.tsx

--- src/component/2.profile/Profile.tsx
+++ src/component/2.profile/Profile.tsx
@@ -1,8 +1,16 @@
+import Button from '../3.features/Button/Button'
+
 const Profile = () => {
     return (
-        <>
-            <span>Profile</span>
-        </>
+        <div>
+          <div>
+            <img src="https://placeimg.com/50/50/people" alt="avatar"/>
+          </div>
+          <div>
+            name: <span>John Wick</span>
+          </div>
+          <Button>Log Out</Button>
+        </div>
     )
 }
 
--- src/component/2.profile/profileReducer.ts
+++ src/component/2.profile/profileReducer.ts
@@ -1,9 +1,9 @@
 const profileInitState = {}
 
 export const profileReducer = (state = profileInitState, action: any): any => {
-    switch (action.type) {
-        default: {
-            return state
-        }
+  switch (action.type) {
+    default: {
+      return state
     }
-};
+  }
+}
