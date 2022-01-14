c6f53dc4d919572392a34dcdb06f0fc7e1a9b238
AlexJHC <shchepkin2021@gmail.com>
2022-01-14 11:08:48 +0200

    add spinner for isLoding status in app

--- src/component/3.features/Loading/Spinner.tsx
+++ src/component/3.features/Loading/Spinner.tsx
@@ -2,7 +2,7 @@ import style from './Spinner.module.css'
 
 const Spinner = () => {
     return (
-        <div>
+        <div className={style.spinnerWrapper}>
             <svg className={style.spinner} viewBox="0 0 100 100" width="80" height="80">
                 <circle cx="50" cy="50" r="42" transform="rotate(-90,50,50)"/>
             </svg>
