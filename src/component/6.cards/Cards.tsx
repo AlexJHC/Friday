90a1992df4ec1f8b85f12d3a878732b4138cfe54
YK177 <kotenev.yuri@gmail.com>
2022-01-18 17:00:54 +0400

    remove useEffect from Cards.tsx

--- src/component/6.cards/Cards.tsx
+++ src/component/6.cards/Cards.tsx
@@ -1,10 +1,10 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {Search} from '../3.features/Search/Search'
 import CardsTable from './CardsTable/CardsTable'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppRootStateType} from '../../store/store'
 import {Pagination} from '../3.features/Pagination/Pagination'
-import {CardsStateType, fetchCards} from '../../store/cardsReducer'
+import {CardsStateType} from '../../store/cardsReducer'
 import {DoubleRange} from '../3.features/DoubleRange/DoubleRange'
 import {Sort} from '../3.features/Sort/Sort'
 
@@ -12,10 +12,6 @@ const Cards = () => {
 
   const dispatch = useDispatch()
   const cardsState = useSelector<AppRootStateType, CardsStateType>(state => state.cards)
-
-  useEffect(() => {
-    dispatch(fetchCards())
-  }, [dispatch])
 
   const onPageChanged = () => {
   }
