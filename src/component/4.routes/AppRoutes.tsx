671cdfdf859f766e1c2a88a1bf3f091b71542594
YK177 <kotenev.yuri@gmail.com>
2022-01-18 15:34:56 +0400

    add route for Cards.tsx

--- src/component/4.routes/AppRoutes.tsx
+++ src/component/4.routes/AppRoutes.tsx
@@ -1,31 +1,33 @@
-import {Navigate, Route, Routes} from "react-router-dom";
-import TestPage from "../1.auth/TestPage";
-import Login from "../1.auth/login/Login";
-import PasswordRestore from "../1.auth/password/passwordRestore/PasswordRestore";
-import CheckEmail from "../1.auth/password/checkEmail/CheckEmail";
-import NewPassword from "../1.auth/password/newPassword/NewPassword";
-import Profile from "../2.profile/Profile";
-import Register from "../1.auth/register/Register";
-import Page404 from "../Page404/Page404";
-import React from "react";
-import {Packs} from "../5.packs/Packs";
+import {Navigate, Route, Routes} from 'react-router-dom'
+import TestPage from '../1.auth/TestPage'
+import Login from '../1.auth/login/Login'
+import PasswordRestore from '../1.auth/password/passwordRestore/PasswordRestore'
+import CheckEmail from '../1.auth/password/checkEmail/CheckEmail'
+import NewPassword from '../1.auth/password/newPassword/NewPassword'
+import Profile from '../2.profile/Profile'
+import Register from '../1.auth/register/Register'
+import Page404 from '../Page404/Page404'
+import React from 'react'
+import {Packs} from '../5.packs/Packs'
+import Cards from '../6.cards/Cards'
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path='/test' element={<TestPage/>}/>
-            <Route path='/' element={<Login/>}/>
-            <Route path='/password-restore' element={<PasswordRestore/>}/>
-            <Route path='/password-on-email' element={<CheckEmail/>}/>
-            <Route path='/create-new-password/:token' element={<NewPassword/>}/>
-            <Route path='/create-new-password' element={<NewPassword/>}/>
-            <Route path='/profile' element={<Profile/>}/>
-            <Route path='/registration' element={<Register/>}/>
-            <Route path='/404' element={<Page404/>}/>
-            <Route path='/packs' element={<Packs/>}/>
-            <Route path={'*'} element={<Navigate to={'/404'}/>}/>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/test" element={<TestPage/>}/>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/password-restore" element={<PasswordRestore/>}/>
+      <Route path="/password-on-email" element={<CheckEmail/>}/>
+      <Route path="/create-new-password/:token" element={<NewPassword/>}/>
+      <Route path="/create-new-password" element={<NewPassword/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/registration" element={<Register/>}/>
+      <Route path="/packs" element={<Packs/>}/>
+      <Route path="/cards" element={<Cards/>}/>
+      <Route path="/404" element={<Page404/>}/>
+      <Route path={'*'} element={<Navigate to={'/404'}/>}/>
+    </Routes>
+  )
 }
 
 export default AppRoutes
