23cb42979d1d64fca1c70f1793fe552d93780a63
YK177 <kotenev.yuri@gmail.com>
2022-01-18 15:46:16 +0400

    add JSX to Cards.tsx

--- src/component/6.cards/Cards.tsx
+++ src/component/6.cards/Cards.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import {Search} from '../3.features/Search/Search'
+import CardsTable from './CardsTable/CardsTable'
+import {useSelector} from 'react-redux'
+import {AppRootStateType} from '../../store/store'
+import {Pagination} from '../3.features/Pagination/Pagination'
+import {CardsStateType} from '../../store/cardsReducer'
 
 const Cards = () => {
+
+  const cardsState = useSelector<AppRootStateType, CardsStateType>(state => state.cards)
+
+  const onPageChanged = () => {
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <Search/>
+      </div>
+      <div>
+        <CardsTable cards={cardsState.cards}/>
+      </div>
+      <div>
+        <Pagination
+          totalRecords={cardsState.cardsTotalCount}
+          pageLimit={cardsState.pageCount}
+          pageNeighbours={3}
+          currentPage={cardsState.page}
+          onPageChanged={onPageChanged}/>
+      </div>
+    </>
   )
 }
 
