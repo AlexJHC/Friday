4d587202cd9a8ac0f9ec07e9788bfb45f13f1a26
AlexJHC <shchepkin2021@gmail.com>
2022-01-18 15:00:23 +0200

    add doubleRange component

--- src/component/3.features/DoubleRange/DoubleRange.tsx
+++ src/component/3.features/DoubleRange/DoubleRange.tsx
@@ -1,11 +1,34 @@
-import { ChangeEvent } from 'react';
+import React, {ChangeEvent} from 'react';
+import style from './DoubleRange.module.css'
 
-export const DoubleRange = () => (
-  // const dispatch = useDispatch();
 
-  <div>
-    Double Range Analogue
-    <input type="number" min="0" />
-    <input type="number" min="10" />
-  </div>
-);
+type DoubleRangePropsType = {
+  onChangeRange: (value: [number, number]) => void
+  value: [number, number]
+}
+
+export const DoubleRange: React.FC<DoubleRangePropsType> = (
+  {
+    onChangeRange, value,
+  }
+) => {
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.name === 'first') {
+      if (+e.currentTarget.value >= 0 && +e.currentTarget.value <= value[1] - 10) {
+        onChangeRange([+e.currentTarget.value, value[1]])
+      }
+    } else if (e.currentTarget.name === 'second') {
+      if (+e.currentTarget.value >= value[0] + 10 && +e.currentTarget.value <= 100)
+        onChangeRange([value[0], +e.currentTarget.value])
+    }
+  }
+
+  return (
+    <div className={style.rangeSlider}>
+      <input type="range" value={value[0]} onChange={changeHandler} name={'first'}/>
+      <input type="range" value={value[1]} onChange={changeHandler} name={'second'}/>
+    </div>
+  )
+}
+
--- src/component/5.packs/Packs.tsx
+++ src/component/5.packs/Packs.tsx
@@ -4,10 +4,21 @@ import {useDispatch, useSelector} from 'react-redux';
 import {fetchPacks} from './../../store/packsReducer';
 import {AppRootStateType} from "../../store/store";
 import {CardPacksType} from "../../api/api-packs";
+import style from './Packs.module.css'
 
 export const Packs = () => {
   const dispatch = useDispatch()
   const packs = useSelector<AppRootStateType, CardPacksType[]>(state => state.packs.cardPacks)
+
+  // Double range
+  const [value1, setValue1] = useState(0)
+  const [value2, setValue2] = useState(100)
+
+  const onChangeSuperRange = (value: [number, number]) => {
+    setValue1(value[0])
+    setValue2(value[1])
+  }
+
 
   // Pagination
   const [currentPage, setCurrentPage] = useState(1);
@@ -25,18 +36,25 @@ export const Packs = () => {
 
 
   return (
-    <>
+    <div className={style.packsWrapper}>
       <div>
         <Search/>
       </div>
-      <div>
-        <DoubleRange/>
-      </div>
-      <div>
-        <Sort/>
-      </div>
-      <div>
-        <PacksTable packs={packs}/>
+      {/*<div>*/}
+      {/*  <Sort/>*/}
+      {/*</div>*/}
+      <div style={{display:"flex", justifyContent: "flex-start", alignItems:"self-start"}}>
+        <div style={{display: "flex", minWidth: '200px', justifyContent: 'center', flexGrow:'1'}}>
+          <span style={{width: '1.4%'}}>{value1}</span>
+          <DoubleRange
+            value={[value1, value2]}
+            onChangeRange={onChangeSuperRange}
+          />
+          <span>{value2}</span>
+        </div>
+        <div>
+          <PacksTable packs={packs}/>
+        </div>
       </div>
       <br/>
       <div>
@@ -48,6 +66,6 @@ export const Packs = () => {
           currentPage={currentPage}
           onPageChanged={onPageChanged}/>
       </div>
-    </>
+    </div>
   );
 };
